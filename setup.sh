1f59a9c6ce971f0edcdf570ee2f0b73c9a18d6ad
Kilian Lieret <kilian.lieret@posteo.de>
2024-04-05 20:19:09 -0400

    Fix docker setup: updated image names

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ echo "Setting up docker image for swe-agent..."
 arch=$(uname -m)
 if [[ "$arch" == "x86_64" ]]; then
   echo "Building the x86 Docker image"
-  docker build -t swe-agent --build-arg MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-py39_23.11.0-1-Linux-x86_64.sh -f docker/swe.Dockerfile .
+  docker build -t sweagent/swe-agent:latest --build-arg MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-py39_23.11.0-1-Linux-x86_64.sh -f docker/swe.Dockerfile .
 elif [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
   echo "Ayy, arm64 in the house!"
-  docker build -t swe-agent --build-arg MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-py39_23.11.0-1-Linux-aarch64.sh -f docker/swe.Dockerfile .
+  docker build -t sweagent/swe-agent:latest --build-arg MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-py39_23.11.0-1-Linux-aarch64.sh -f docker/swe.Dockerfile .
 else
   echo "unknown architecture detected?"
   echo $arch
@@ -20,6 +20,6 @@ fi
 
 # build eval.Dockerfile
 echo "Setting up docker image for evaluation..."
-docker build -t swe-eval -f docker/eval.Dockerfile .
+docker build -t sweagent/swe-eval:latest -f docker/eval.Dockerfile .
 
 echo "Done with setup!"
