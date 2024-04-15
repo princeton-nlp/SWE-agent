bf70fcee14b691f8cecab61cbaab23ccb453d0bc
talentoscope <22162766+talentoscope@users.noreply.github.com>
2024-04-16 00:05:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@
 # bash strict mode
 set -euo pipefail
 
+# Detect the architecture of the host system and export it
+export ARCH=$(uname -m)
+
 echo "Setting up docker image for swe-agent..."
-docker build -t sweagent/swe-agent:latest -f docker/swe.Dockerfile .
+docker build --build-arg TARGETARCH="$ARCH" -t sweagent/swe-agent:latest -f docker/swe.Dockerfile .
 
 echo "Setting up docker image for evaluation..."
-docker build -t sweagent/swe-eval:latest -f docker/eval.Dockerfile .
+docker build --build-arg TARGETARCH="$ARCH" -t sweagent/swe-eval:latest -f docker/eval.Dockerfile ../
 
 echo "Done with setup!"
