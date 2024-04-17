b016d39bf8c32c571e1ddea8569954d7eeb8db24
mikanfactory <k952i4j14x17@gmail.com>
2024-04-17 22:38:10 +0900

    test: add unit test for anthropic, ollama, human model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 from unittest.mock import MagicMock, Mock, patch
-from sweagent.agent.models import OpenAIModel, ModelArguments
+from sweagent.agent.models import OpenAIModel, ModelArguments, AnthropicModel, OllamaModel, HumanModel
 import pytest
 
 
@@ -17,6 +17,26 @@ def openai_mock_client():
     return model
 
 
+def anthropic_return_value():
+    return Mock(
+        id="test",
+        completion="Hello. I'm fine.",
+        model="claude-haiku",
+        stop_reason=None,
+        type="completion"
+    )
+
+
+def ollama_return_value():
+    return {
+        "prompt_eval_count": 10,
+        "eval_count": 10,
+        "message": {
+            "content": "Hello. I'm fine."
+        }
+    }
+
+
 TEST_HISTORY = [
     {
         "role": "system",
@@ -29,6 +49,34 @@ def test_openai_model(openai_mock_client):
     for model_name in list(OpenAIModel.MODELS) + list(OpenAIModel.SHORTCUTS):
         TEST_MODEL_ARGUMENTS = ModelArguments(model_name)
         with patch("sweagent.agent.models.config.Config"), patch("sweagent.agent.models.OpenAI"):
-                model = OpenAIModel(TEST_MODEL_ARGUMENTS, [])
+            model = OpenAIModel(TEST_MODEL_ARGUMENTS, [])
         model.client = openai_mock_client
         model.query(TEST_HISTORY)
+
+
+@patch("anthropic.resources.completions.Completions.create", return_value=anthropic_return_value())
+def test_anthropic_model(mock):
+    model_name = "claude"
+    model_arguments = ModelArguments(model_name)
+    with patch("sweagent.agent.models.config.Config"):
+        model = AnthropicModel(model_arguments, [])
+
+    model.query(TEST_HISTORY)
+
+
+@patch("ollama.Client.chat", return_value=ollama_return_value())
+def test_ollama_model(mock):
+    model_arguments = ModelArguments("ollama:llama2")
+    with patch("sweagent.agent.models.config.Config"):
+        model = OllamaModel(model_arguments, [])
+
+    model.query(TEST_HISTORY)
+
+
+@patch("builtins.input", return_value="Hello, I'm fine.")
+def test_human_model(mock):
+    model_arguments = ModelArguments("human")
+    with patch("sweagent.agent.models.config.Config"):
+        model = HumanModel(model_arguments, [])
+
+    model.query(TEST_HISTORY)
