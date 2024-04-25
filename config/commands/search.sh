13fb4fffd8ee2511bd2cba4406bf694381fa5d4e
Khang Pham <khangphama@gmail.com>
2024-04-25 15:45:47 -0700

    use git grep for faster search

--- config/commands/search.sh
+++ config/commands/search.sh
@@ -11,40 +11,40 @@
 #     description: the directory to search in (if not provided, searches in the current directory)
 #     required: false
 search_dir() {
-    if [ $# -eq 1 ]; then
-        local search_term="$1"
-        local dir="./"
-    elif [ $# -eq 2 ]; then
-        local search_term="$1"
-        if [ -d "$2" ]; then
-            local dir="$2"
-        else
-            echo "Directory $2 not found"
-            return
-        fi
-    else
+    if [ $# -lt 1 ] || [ $# -gt 2 ]; then
         echo "Usage: search_dir <search_term> [<dir>]"
         return
     fi
-    dir=$(realpath "$dir")
-    local matches=$(find "$dir" -type f ! -path '*/.*' -exec grep -nIH -- "$search_term" {} + | cut -d: -f1 | sort | uniq -c)
-    # if no matches, return
+
+    local search_term="$1"
+    local dir="${2:-.}" # Use current directory if not specified
+
+    # Change to the specified directory if provided
+    if [ -n "$2" ] && [ -d "$2" ]; then
+        cd "$2" || return
+    elif [ -n "$2" ]; then
+        echo "Directory $2 not found"
+        return
+    fi
+
+    # Use git grep to search for the term
+    local matches=$(git grep -n "$search_term")
+
     if [ -z "$matches" ]; then
         echo "No matches found for \"$search_term\" in $dir"
         return
     fi
-    # Calculate total number of matches
-    local num_matches=$(echo "$matches" | awk '{sum+=$1} END {print sum}')
-    # calculate total number of files matched
-    local num_files=$(echo "$matches" | wc -l | awk '{$1=$1; print $0}')
-    # if num_files is > 100, print an error
+
+    local num_matches=$(echo "$matches" | wc -l | awk '{$1=$1; print $0}')
+    local num_files=$(echo "$matches" | cut -d: -f1 | sort | uniq | wc -l | awk '{$1=$1; print $0}')
+
     if [ $num_files -gt 100 ]; then
         echo "More than $num_files files matched for \"$search_term\" in $dir. Please narrow your search."
         return
     fi
-    
+
     echo "Found $num_matches matches for \"$search_term\" in $dir:"
-    echo "$matches" | awk '{$2=$2; gsub(/^\.+\/+/, "./", $2); print $2 " ("$1" matches)"}'
+    echo "$matches" | awk -F: '{print $1 " (" $2 " matches)"}'
     echo "End of matches for \"$search_term\" in $dir"
 }
 
