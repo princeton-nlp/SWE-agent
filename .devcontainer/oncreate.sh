47257081ec5477d7c41d9a441169598481d173b4
Kilian Lieret <kilian.lieret@posteo.de>
2024-05-01 19:20:12 -0400

    Improve codespace setup & documentation (#293)
    [skip-CI]
    
    * Codespaces: Remove shell setting; fix extensions setting
    
    [skip-ci]
    
    * Codespaces: Copy sample keys.cfg
    
    [skip-ci]
    
    * Codespaces: Add codespace badge
    
    [skip-CI]

--- .devcontainer/oncreate.sh
+++ .devcontainer/oncreate.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 set -x
 
+# Run from repo root
+
 sudo usermod -aG docker vscode
 sudo chmod 666 /var/run/docker.sock
 pip install -e '.'
+cp .devcontainer/sample_keys.cfg keys.cfg
