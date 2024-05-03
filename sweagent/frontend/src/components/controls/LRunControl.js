0419f53798180a3784291321e04d7a91ef84af2d
Kilian Lieret <kilian.lieret@posteo.de>
2024-05-03 18:30:18 -0400

    Web: Place controls in accordion

--- sweagent/frontend/src/components/controls/LRunControl.js
+++ sweagent/frontend/src/components/controls/LRunControl.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 function LRunControl({isComputing, isConnected, handleStop, handleSubmit, setDataPath, setTestRun, dataPath, testRun, repoPath, setRepoPath}) {
   const [psType, setPsType] = useState('gh');
@@ -32,26 +33,38 @@ function LRunControl({isComputing, isConnected, handleStop, handleSubmit, setDat
 
   return (
     <div>
-      <div class="input-group mb-3">
-        <span class="input-group-text" >Problem statement</span>
-        <select class="form-select" aria-label="Select problem statement type" onChange={handlePsTypeChange} >
-          <option value="gh" selected>GitHub issue URL</option>
-          <option value="write">Write here</option>
-        </select>
-        {getPsInput()}
-      </div>
-      <div class="input-group mb-3">
-        <span class="input-group-text" >Repository path</span>
-        <input type="text" class="form-control" placeholder="Enter repository path (optional when using GitHub issue)" onChange={(e) => setRepoPath(e.target.value)}  /> 
-      </div>
-      {/* <input type="text" value={dataPath} onChange={(e) => setDataPath(e.target.value)} required /> */}
-      <label htmlFor="test_run">Test run (no LM queries)</label>
-      <input type="checkbox" checked={testRun} onChange={(e) => setTestRun(e.target.checked)} />
-      <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="submit" className="btn btn-primary" onClick={handleSubmit} disabled={isComputing || !isConnected}>Run</button>
-        <button onClick={handleStop} disabled={!isComputing} className="btn btn-primary">Stop</button>
-      </div>
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Problem statement</Accordion.Header>
+        <Accordion.Body>
+            <div class="input-group mb-3">
+              <span class="input-group-text" >Problem statement</span>
+              <select class="form-select" aria-label="Select problem statement type" onChange={handlePsTypeChange} >
+                <option value="gh" selected>GitHub issue URL</option>
+                <option value="write">Write here</option>
+              </select>
+              {getPsInput()}
+            </div>
+            <div class="input-group mb-3">
+              <span class="input-group-text" >Repository path</span>
+              <input type="text" class="form-control" placeholder="Enter repository path (optional when using GitHub issue)" onChange={(e) => setRepoPath(e.target.value)}  /> 
+            </div>
+            {/* <input type="text" value={dataPath} onChange={(e) => setDataPath(e.target.value)} required /> */}
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Extra settings</Accordion.Header>
+        <Accordion.Body>
+            <label htmlFor="test_run">Test run (no LM queries)</label>
+            <input type="checkbox" checked={testRun} onChange={(e) => setTestRun(e.target.checked)} />
+          </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="submit" className="btn btn-primary" onClick={handleSubmit} disabled={isComputing || !isConnected}>Run</button>
+      <button onClick={handleStop} disabled={!isComputing} className="btn btn-primary">Stop</button>
     </div>
+  </div>
   );
 }
 
