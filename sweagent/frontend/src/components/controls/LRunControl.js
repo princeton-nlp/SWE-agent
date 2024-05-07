d62bfeead3a82dc12cc32eea8f9a734f712a7a2f
Kilian Lieret <kilian.lieret@posteo.de>
2024-05-07 12:29:55 -0400

    Web: Show explicit support for local PS or repos

--- sweagent/frontend/src/components/controls/LRunControl.js
+++ sweagent/frontend/src/components/controls/LRunControl.js
@@ -29,6 +29,18 @@ function LRunControl({isComputing, isConnected, handleStop, handleSubmit, setDat
         </div>
       );
     }
+    if (psType === 'local') {
+      return (
+        <div className="input-group mb-3">
+          <span className="input-group-text">Path to local .md or .txt file with problem statement</span>
+          <input 
+            type="text" 
+            className="form-control" 
+            onChange={(e) => setDataPath(e.target.value)}
+            placeholder="/path/to/your/local/file.md"/>
+        </div>
+      );
+    }
     if (psType === 'write') {
       return (
         <textarea 
@@ -63,6 +75,7 @@ function LRunControl({isComputing, isConnected, handleStop, handleSubmit, setDat
               <span className="input-group-text">Problem source</span>
               <select className="form-select" aria-label="Select problem statement type" onChange={handlePsTypeChange}>
                 <option value="gh">GitHub issue URL</option>
+                <option value="local">Local file</option>
                 <option value="write">Write here</option>
               </select>
             </div>
@@ -70,8 +83,8 @@ function LRunControl({isComputing, isConnected, handleStop, handleSubmit, setDat
               {getPsInput()}
             </div>
             <div className="input-group mb-3">
-              <span className="input-group-text">Repository path</span> 
-              <input type="text" className="form-control" placeholder="Optional when using GitHub issue" onChange={(e) => setRepoPath(e.target.value)} />
+              <span className="input-group-text">Local repo path or GitHub URL</span> 
+              <input type="text" className="form-control" placeholder="Optional when using GitHub issue as problem source" onChange={(e) => setRepoPath(e.target.value)} />
             </div>
           </div>
         </Tab>
