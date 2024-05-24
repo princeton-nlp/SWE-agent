58b665fecc6326d8b153a9b03888f8d2a8da0786
Kilian Lieret <kilian.lieret@posteo.de>
2024-05-24 17:45:15 -0400

    Ensure that token is passed

--- sweagent/environment/swe_env.py
+++ sweagent/environment/swe_env.py
@@ -917,6 +917,7 @@ class SWEEnv(gym.Env):
             f"to close [#{issue.number}]({issue_url}) ({issue.title}).\n\nCloses #{issue.number}."
         )
         body += "\n\n" + format_trajectory_markdown(trajectory)
+        assert self._github_token
         api = GhApi(token=self._github_token)
         if not _dry_run:
             pr_info = api.pulls.create(
