6866b34048fa4528025106d0e4e10183b5ea50a6
ethanabrooks <ethanabrooks@gmail.com>
2024-06-10 19:10:42 +0000

    add nix stuff

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/";
+    utils.url = "github:numtide/flake-utils/";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }: let
+    out = system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          alejandra
+          poetry
+          python3Full
+        ];
+        PYTHONBREAKPOINT = "ipdb.set_trace";
+        shellHook = ''
+          export LD_LIBRARY_PATH=${pkgs.zlib.out}/lib:${pkgs.libgcc.lib}/lib:$LD_LIBRARY_PATH
+          set -o allexport
+          source .env
+          set +o allexport
+        '';
+      };
+    };
+  in
+    utils.lib.eachDefaultSystem out;
+}
