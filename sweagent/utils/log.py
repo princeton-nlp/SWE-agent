ce98ca442e685c8c801b9ca1238dc8e6b0b173b8
Kilian Lieret <kilian.lieret@posteo.de>
2024-06-18 18:35:31 -0400

    Enh: Always write log to traj directory (#588)
    Closes #581

--- sweagent/utils/log.py
+++ sweagent/utils/log.py
@@ -2,13 +2,18 @@ from __future__ import annotations
 
 import logging
 import os
+from pathlib import PurePath
 
 from rich.logging import RichHandler
 
 _SET_UP_LOGGERS = set()
+_ADDITIONAL_HANDLERS = []
 
 
 def get_logger(name: str) -> logging.Logger:
+    """Get logger. Use this instead of `logging.getLogger` to ensure
+    that the logger is set up with the correct handlers.
+    """
     logger = logging.getLogger(name)
     if name in _SET_UP_LOGGERS:
         # Already set up
@@ -22,7 +27,23 @@ def get_logger(name: str) -> logging.Logger:
     logger.addHandler(handler)
     logger.propagate = False
     _SET_UP_LOGGERS.add(name)
+    for handler in _ADDITIONAL_HANDLERS:
+        logger.addHandler(handler)
     return logger
+
+
+def add_file_handler(path: PurePath | str) -> None:
+    """Adds a file handler to all loggers that we have set up
+    and all future loggers that will be set up with `get_logger`.
+    """
+    handler = logging.FileHandler(path)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    for name in _SET_UP_LOGGERS:
+        logger = logging.getLogger(name)
+        logger.addHandler(handler)
+    _ADDITIONAL_HANDLERS.append(handler)
 
 
 default_logger = get_logger("swe-agent")
