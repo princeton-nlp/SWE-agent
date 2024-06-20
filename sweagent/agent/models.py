61ca1be98a63677c3b1d1d08bf9630c42977748d
Ofir Press <ofirp@cs.washington.edu>
2024-06-20 13:31:54 -0400

    Update models.py - fixing error message for azure --model issue (#597)
    * Update models.py - fixing error message for azure --model issue
    
    * Fix message
    
    ---------
    
    Co-authored-by: Kilian Lieret <kilian.lieret@posteo.de>

--- sweagent/agent/models.py
+++ sweagent/agent/models.py
@@ -247,7 +247,8 @@ class OpenAIModel(BaseModel):
             if "model" in self.args and self.args.model:
                 logger.warning(
                     "The --model CLI argument is ignored when using the Azure GPT endpoint. "
-                    "The model is determined by AzureOpenAI.",
+                    "The model is determined by the AZURE_OPENAI_DEPLOYMENT key/"
+                    "environment variable (this might change in the future).",
                 )
             self.api_model = keys_config["AZURE_OPENAI_DEPLOYMENT"]
             self.client = AzureOpenAI(
