a795862c85cb4d96b5b51013bf2f4eab0d47346e
Kilian Lieret <kilian.lieret@posteo.de>
2024-06-24 16:31:16 -0400

    Make log level configurable; add TRACE level (#612)
    Closes #546

--- sweagent/utils/log.py
+++ sweagent/utils/log.py
@@ -9,6 +9,21 @@ from rich.logging import RichHandler
 _SET_UP_LOGGERS = set()
 _ADDITIONAL_HANDLERS = []
 
+logging.TRACE = 5  # type: ignore
+logging.addLevelName(logging.TRACE, "TRACE")  # type: ignore
+
+
+def _interpret_level_from_env(level: str | None) -> int:
+    if not level:
+        return logging.DEBUG
+    if level.isnumeric():
+        return int(level)
+    return getattr(logging, level.upper(), logging.DEBUG)
+
+
+_STREAM_LEVEL = _interpret_level_from_env(os.environ.get("SWE_AGENT_LOG_STREAM_LEVEL"))
+_FILE_LEVEL = _interpret_level_from_env(os.environ.get("SWE_AGENT_LOG_FILE_LEVEL"))
+
 
 def get_logger(name: str) -> logging.Logger:
     """Get logger. Use this instead of `logging.getLogger` to ensure
@@ -22,8 +37,8 @@ def get_logger(name: str) -> logging.Logger:
         show_time=bool(os.environ.get("SWE_AGENT_LOG_TIME", False)),
         show_path=False,
     )
-    handler.setLevel(logging.DEBUG)
-    logger.setLevel(logging.DEBUG)
+    handler.setLevel(_STREAM_LEVEL)
+    logger.setLevel(min(_STREAM_LEVEL, _FILE_LEVEL))
     logger.addHandler(handler)
     logger.propagate = False
     _SET_UP_LOGGERS.add(name)
@@ -39,7 +54,7 @@ def add_file_handler(path: PurePath | str) -> None:
     handler = logging.FileHandler(path)
     formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
     handler.setFormatter(formatter)
-    handler.setLevel(logging.DEBUG)
+    handler.setLevel(_FILE_LEVEL)
     for name in _SET_UP_LOGGERS:
         logger = logging.getLogger(name)
         logger.addHandler(handler)
