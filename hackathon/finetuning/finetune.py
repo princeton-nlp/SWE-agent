3b24803ea977cb89703726f968da5cba0a41bd29
kjobanputra <kunjoban@gmail.com>
2024-08-03 17:26:35 -0400

    multiline string in python + append to file instead of overwrite

--- hackathon/finetuning/finetune.py
+++ hackathon/finetuning/finetune.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 # Currently, this file just contains a function that will create training dataset to finetune on.
 # Generated file from `create_training_data` can be uploaded to OpenPipe console to finetune the model.
@@ -27,9 +28,9 @@ class Finetune():
                     #   in the user message, but the instructions set in the 
                     #   system message are more effective." 
                     "content": 
-                    "You are a seasoned engineer who takes bug descriptions \
-                    and generates a patch to fix the bug. For this bug, you \
-                    are provided with the following hint(s): " + one_json_obj["hints_text"],
+                    """You are a seasoned engineer who takes bug descriptions 
+                    and generates a patch to fix the bug. For this bug, you 
+                    are provided with the following hint(s): """ + one_json_obj["hints_text"],
                 },
                 {
                     "role": "user",
@@ -44,17 +45,18 @@ class Finetune():
 
     # Upload this output to OpenPipe on console
     def create_training_data(self, jsonData, model = "gpt-4o-mini"):
-        f = open("training_data.jsonl", "w")
+        f = open("hackathon/finetuning/training_data.jsonl", "a")
         for node in jsonData:
             # write output of transform_single_to_openai_chat_format in a single line to jsonl file
             f.write(str(self.transform_single_to_openai_chat_format(node, model)) + "\n")
         f.close()
 
 if __name__ == "__main__":
+    print(os.getcwd())
     # Change this to where the data is stored
-    filePath = "../../tests/test_data/data_sources/swe-bench-dev-easy.json"
+    filePath = "tests/test_data/data_sources/swe-bench-dev-easy.json"
     fileObj = open(filePath)
     jsonData = json.load(fileObj)
 
-    finetuneObj = Finetune()
-    finetuneObj.create_training_data(jsonData)
+    Finetune().create_training_data(jsonData)
+    
