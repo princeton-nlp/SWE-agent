572f8aa3588844dfeb092f54b283e5338908c0d2
Kilian Lieret <kilian.lieret@posteo.de>
2024-11-25 12:31:06 -0500

    Raise if human model with multithreading

--- sweagent/run/run_batch.py
+++ sweagent/run/run_batch.py
@@ -90,6 +90,10 @@ class RunBatch:
             hooks: If not specified, the default hooks will be used.
             num_workers: Number of parallel workers to use. Default is 1 (sequential execution).
         """
+        if agent_config.model.name in ["human", "human_thought"] and num_workers > 1:
+            msg = "Cannot run with human model in parallel"
+            raise ValueError(msg)
+
         self.logger = get_logger("swea-run", emoji="🏃")
         add_file_handler(
             output_dir / "run_batch.log",
