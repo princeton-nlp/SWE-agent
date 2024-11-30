8fd952e5449916810be78ed705303dd358711fde
Kilian Lieret <kilian.lieret@posteo.de>
2024-11-30 18:52:53 -0500

    Enh(run_batch): Display two full ids in exit status table

--- sweagent/run/_progress.py
+++ sweagent/run/_progress.py
@@ -90,7 +90,7 @@ class RunBatchProgressManager:
             t.show_header = True
             # self._exit_status_table.rows.clear()
             for status, instances in self._instances_by_exit_status.items():
-                instances_str = _shorten_str(", ".join(reversed(instances)), 40)
+                instances_str = _shorten_str(", ".join(reversed(instances)), 55)
                 t.add_row(status, str(len(instances)), instances_str)
         assert self.render_group is not None
         self.render_group.renderables[0] = t
