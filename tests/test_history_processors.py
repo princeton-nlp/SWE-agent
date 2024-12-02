4f76380f6eca85c70183ef56ef092d9181de845c
Kilian Lieret <kilian.lieret@posteo.de>
2024-12-02 14:38:52 -0500

    CI/Ref: Refactor test_last_n_observations

--- tests/test_history_processors.py
+++ tests/test_history_processors.py
@@ -4,18 +4,22 @@ from pathlib import Path
 from sweagent.agent.history_processors import LastNObservations
 
 
+def get_history(traj_path: Path):
+    return json.loads((traj_path).read_text())["history"]
+
+
+def count_elided_observations(history: list[dict]):
+    return len([entry for entry in history if "Old environment output" in entry["content"]])
+
+
 def test_last_n_observations(test_trajectories_path: Path):
-    history = json.loads(
-        (
-            test_trajectories_path
-            / "gpt4__swe-agent-test-repo__default_from_url__t-0.00__p-0.95__c-3.00__install-1/6e44b9__sweagenttestrepo-1c2844.traj"
-        ).read_text()
-    )["history"]
+    history = get_history(
+        test_trajectories_path
+        / "gpt4__swe-agent-test-repo__default_from_url__t-0.00__p-0.95__c-3.00__install-1/6e44b9__sweagenttestrepo-1c2844.traj"
+    )
     processor = LastNObservations(n=3)
     new_history = processor(history)
     total_observations = len([entry for entry in history if entry["message_type"] == "observation"])
     # extra -1 because instance template is kept
-    assert (
-        len([entry for entry in new_history if "Old environment output" in entry["content"]])
-        == total_observations - 3 - 1
-    )
+    expected_elided_observations = total_observations - 3 - 1
+    assert count_elided_observations(new_history) == expected_elided_observations
