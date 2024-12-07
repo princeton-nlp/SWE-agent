c951d79e1bb56e0e7860cef13bf28ca315ca9d66
Kilian Lieret <kilian.lieret@posteo.de>
2024-12-07 18:24:16 -0500

    Fix platform dependent tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -10,7 +10,7 @@ def test_run_cli_no_arg_error():
     output = subprocess.run(args, check=False, capture_output=True)
     print(output.stdout.decode())
     print(output.stderr.decode())
-    assert output.returncode == 2
+    assert output.returncode in [1, 2]
     assert "run-batch" in output.stdout.decode()
     assert "run-replay" in output.stdout.decode()
     assert "run" in output.stdout.decode()
@@ -22,7 +22,6 @@ def test_run_cli_main_help():
         "--help",
     ]
     output = subprocess.run(args, check=True, capture_output=True)
-    assert output.returncode == 0
     assert "run-batch" in output.stdout.decode()
     assert "run-replay" in output.stdout.decode()
     assert "run" in output.stdout.decode()
@@ -35,5 +34,4 @@ def test_run_cli_subcommand_help():
         "--help",
     ]
     output = subprocess.run(args, check=True, capture_output=True)
-    assert output.returncode == 0
     assert "--config" in output.stdout.decode()
