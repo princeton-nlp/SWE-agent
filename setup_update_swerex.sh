674bbe9cf7de975718194c9bbef6a42273b5bb8d
Kilian Lieret <kilian.lieret@posteo.de>
2024-12-06 23:04:32 -0500

    Remove legacy scripts

--- setup_update_swerex.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# bash strict mode
-set -euo pipefail
-
-docker build -t sweagent/swe-agent:latest -f SWE-agent/docker/update_swe_rex.Dockerfile --build-arg TARGETARCH=$(uname -m) .
-
-echo "Done with setup!"
